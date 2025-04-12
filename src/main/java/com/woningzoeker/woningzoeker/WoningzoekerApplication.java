c136703f560d5ccfa96c8c33da4434da41338423
M Heynen <20039274+MHeynen97@users.noreply.github.com>
2025-04-12 11:07:21 +0200

    StartProject
    Springboot basis programmatuur

---
+++ src/main/java/com/woningzoeker/woningzoeker/WoningzoekerApplication.java
@@ -0,0 +1,13 @@
+package com.woningzoeker.woningzoeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoningzoekerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoningzoekerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woningzoeker/woningzoeker/WoningzoekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woningzoeker.woningzoeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoningzoekerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
