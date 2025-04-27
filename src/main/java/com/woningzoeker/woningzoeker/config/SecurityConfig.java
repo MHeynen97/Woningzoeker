c1982314eaca18a7f3d62a70c5447325ae1fecf9
M Heynen <20039274+MHeynen97@users.noreply.github.com>
2025-04-27 14:11:19 +0200

    Opzet security classes

---
+++ src/main/java/com/woningzoeker/woningzoeker/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.woningzoeker.woningzoeker.config;
+
+import com.woningzoeker.woningzoeker.security.JwtRequestFilter;
+import com.woningzoeker.woningzoeker.security.JwtService;
+import com.woningzoeker.woningzoeker.services.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtService jwtService) throws Exception {
+
+        http
+                .httpBasic(hp -> hp.disable()) //aanpassing
+                .authorizeHttpRequests(auth -> auth
+                        // Publieke endpoints
+                        .requestMatchers("/api-docs/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/login").permitAll() //aanpassing
+
+                        .requestMatchers(HttpMethod.GET, "/cars").hasAnyRole("USER")
+                        // Beveiligde endpoints: Alleen voor admins
+                        .requestMatchers("/cars/**").hasRole("ADMIN")
+
+                        // Andere verzoeken worden geweigerd
+                        .anyRequest().denyAll()
+                )
+                .addFilterBefore(new JwtRequestFilter(jwtService), UsernamePasswordAuthenticationFilter.class) // aanpassing
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        ;
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        var builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+        return builder.build();
+    }
+}
---
+++ src/main/java/com/woningzoeker/woningzoeker/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.woningzoeker.woningzoeker.services;
+
+import com.woningzoeker.woningzoeker.models.Gebruiker;
+import com.woningzoeker.woningzoeker.repositories.UserRepository;
+import nl.novi.cardemo.models.User;
+import nl.novi.cardemo.repositories.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Gebruiker user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Gebruiker niet gevonden: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
